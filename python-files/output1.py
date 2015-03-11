62fcabd9178d49e7ef01efd2d852242d401ba63e
chandan <chandan.may03@gmail.com>
2015-03-11 13:37:43 +0530

    python-files folder added

---
+++ python-files/output1.py
@@ -0,0 +1,42 @@
+test_g2w_mod_unmute_lis_bb_12439
+
+moderator unmutes listener before broadcast is started
+-------------------------Files and Prompts----------------------
+Organizer call info: {'phone_number': '18054445191', 'call_time': '2015-02-24 22:16:02', 'edge_server_ip': '10.39.52.92', 'channel_id': '02C8D593-F1D7-4871-AA0FB92819F0E803', 'conference_id': 'c69b1cdf-c5c2-42ac-8276-7268ce1cfdd9', 'call_id': '02c8d593-f1d7-4871-aa0f-b92819f0e803'}
+Panelist call info:  {'phone_number': '18054445191', 'call_time': '2015-02-24 22:16:02', 'edge_server_ip': '10.39.52.92', 'channel_id': 'C0DFB019-FBE5-4A89-844F2A5C7A552389', 'conference_id': 'c69b1cdf-c5c2-42ac-8276-7268ce1cfdd9', 'call_id': 'c0dfb019-fbe5-4a89-844f-2a5c7a552389'}
+Listener call info:  {'phone_number': '18054445191', 'call_time': '2015-02-24 22:16:02', 'edge_server_ip': '10.39.52.92', 'channel_id': '82224711-B3B7-4DC0-9CEC328B5C33CEEE', 'conference_id': 'c69b1cdf-c5c2-42ac-8276-7268ce1cfdd9', 'call_id': '82224711-b3b7-4dc0-9cec-328b5c33ceee'}
+-------------------------Files and Prompts----------------------
+
+{
+'150225-061607-conference_C69B1CDF-C5C2-42AC-82767268CE1CFDD9/MixerOutChannel_channeluuid-C0DFB019-FBE5-4A89-844F2A5C7A552389_codec-ISAC_rate-16000_frame-480_fid-2.16000.wav': 
+            ('', '000000000', -876640), 
+
+'150225-061607-conference_C69B1CDF-C5C2-42AC-82767268CE1CFDD9/MixerSumStream_8khz_fid-0.16000.wav': 
+            ('A', '000000000', -1110602), 
+
+'150225-061607-conference_C69B1CDF-C5C2-42AC-82767268CE1CFDD9/MixerSumStream_16khz_fid-1.16000.wav': 
+            ('A', '000000000', -1110602), 
+            
+'150225-061607-conference_C69B1CDF-C5C2-42AC-82767268CE1CFDD9/MixerOutChannel_channeluuid-02C8D593-F1D7-4871-AA0FB92819F0E803_codec-ISAC_rate-16000_frame-480_fid-5.16000.wav': 
+            ('', '000000000', -782561)
+            
+}
+
+
+prompt 150225-061607-conference_C69B1CDF-C5C2-42AC-82767268CE1CFDD9/MixerOutChannel_channeluuid-C0DFB019-FBE5-4A89-844F2A5C7A552389_codec-ISAC_rate-16000_frame-480_fid-2.16000.wav is correct 
+played prompt is  
+
+
+prompt 150225-061607-conference_C69B1CDF-C5C2-42AC-82767268CE1CFDD9/MixerSumStream_8khz_fid-0.16000.wav is correct 
+played prompt is  A
+
+prompt 150225-061607-conference_C69B1CDF-C5C2-42AC-82767268CE1CFDD9/MixerSumStream_16khz_fid-1.16000.wav is correct 
+played prompt is  A
+
+
+prompt 150225-061607-conference_C69B1CDF-C5C2-42AC-82767268CE1CFDD9/MixerOutChannel_channeluuid-02C8D593-F1D7-4871-AA0FB92819F0E803_codec-ISAC_rate-16000_frame-480_fid-5.16000.wav is correct 
+played prompt is  
+
+
+Deleted... 150225-061607-conference_C69B1CDF-C5C2-42AC-82767268CE1CFDD9
+delete ..., res= 204 No Content
---
+++ python-files/output3.py
@@ -0,0 +1,43 @@
+150303-044017-conference_34B6A325-4429-40DB-9AE5FF7402FD7189/MixerOutChannel_channeluuid-A3E13017-B9F9-481A-BC79D7440E4FEEB8_codec-G711U_rate-8000_frame-160_fid-2.wav
+-------------------------Conference details----------------------
+Organizer call info: 
+        {
+         'phone_number': '16035408033', 
+         'call_time': '2015-03-02 20:40:13', 
+         'edge_server_ip': '10.39.52.92', 
+         'channel_id': 'F73E4419-3E7D-44D8-846E5907C2C6B29B', 
+         'conference_id': '34b6a325-4429-40db-9ae5-ff7402fd7189', 
+         'call_id': 'f73e4419-3e7d-44d8-846e-5907c2c6b29b'
+         }
+Panelist call info: 
+        {
+         'phone_number': '16035408033', 
+         'call_time': '2015-03-02 20:40:18', 
+         'edge_server_ip': '10.39.52.92', 
+         'channel_id': 'A3E13017-B9F9-481A-BC79D7440E4FEEB8', 
+         'conference_id': '34b6a325-4429-40db-9ae5-ff7402fd7189', 
+         'call_id': 'a3e13017-b9f9-481a-bc79-d7440e4feeb8'
+         }
+Listener call info: 
+        {'phone_number': '16035408033', 'call_time': '2015-03-02 20:40:23', 'edge_server_ip': '10.39.52.92', 'channel_id': 'AE420C98-6BE7-4D6F-B781C0EA4C05922B', 'conference_id': '34b6a325-4429-40db-9ae5-ff7402fd7189', 'call_id': 'ae420c98-6be7-4d6f-b781-c0ea4c05922b'}
+-------------------------Files and Prompts----------------------
+{
+
+'150303-044017-conference_34B6A325-4429-40DB-9AE5FF7402FD7189/MixerOutChannel_channeluuid-A3E13017-B9F9-481A-BC79D7440E4FEEB8_codec-G711U_rate-8000_frame-160_fid-2.16000.wav': 
+        ('A', '000000000', -499810), 
+
+'150303-044017-conference_34B6A325-4429-40DB-9AE5FF7402FD7189/MixerOutChannel_channeluuid-F73E4419-3E7D-44D8-846E5907C2C6B29B_codec-G711U_rate-8000_frame-160_fid-5.16000.wav': 
+        ('A', '000000000', -409771), 
+        
+'150303-044017-conference_34B6A325-4429-40DB-9AE5FF7402FD7189/MixerSumStream_8khz_fid-0.16000.wav': 
+        ('A', '000000000', -499914), 
+        
+'150303-044017-conference_34B6A325-4429-40DB-9AE5FF7402FD7189/MixerSumStream_16khz_fid-1.16000.wav': 
+        ('A', '000000000', -499914)
+        
+}
+delete ..., res= 204 No Content
+ok
+
+----------------------------------------------------------------------
+Ran 1 test in 3566.359s
